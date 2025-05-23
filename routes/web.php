a46712ecce845c90b1128abaa8d95b23b1d3812b
AviroVins <avirovins@gmail.com>
2025-05-23 15:54:25 +0200

    Last

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
+use App\Http\Kernel;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,13 +21,16 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+
     Route::get('/admin/table/{name}', [AdminController::class, 'showTable'])->name('admin.table');
-    
+
     Route::get('/admin/table/{name}/create', [AdminController::class, 'create'])->name('admin.table.create');
     Route::post('/admin/table/{name}', [AdminController::class, 'store'])->name('admin.table.store');
-    Route::get('/admin/table/{name}/edit/{id}', [AdminController::class, 'edit'])->name('admin.table.edit');
-    Route::put('/admin/table/{name}/update/{id}', [AdminController::class, 'update'])->name('admin.table.update');
-    Route::delete('/admin/table/{name}/delete/{id}', [AdminController::class, 'destroy'])->name('admin.table.delete');
+
+    Route::get('/admin/table/{name}/{id}/edit', [AdminController::class, 'edit'])->name('admin.table.edit');
+    Route::put('/admin/table/{name}/{id}', [AdminController::class, 'update'])->name('admin.table.update');
+
+    Route::delete('/admin/table/{name}/{id}', [AdminController::class, 'destroy'])->name('admin.table.delete');
 });
 
 require __DIR__.'/auth.php';
